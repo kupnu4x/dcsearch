175ec592481df19d3e4ec63dbbeee7c9819b9cf4
kupnu4x@gmail.com <kupnu4x@gmail.com>
2010-03-11 18:02:42 +0000

    hourly->delta weekly->main install readme

--- scripts/hourly.bat
+++ scripts/hourly.bat
@@ -1,5 +1,5 @@
 cd C:\work\dcsearch\
 C:\work\bin\php-5.3\php.exe C:\work\dcsearch\lister.php
 C:\work\bin\php-5.3\php.exe C:\work\dcsearch\processer.php
-C:\work\dcsearch\scripts\indexer.main.bat
+C:\work\dcsearch\scripts\indexer.delta.bat
 pause
