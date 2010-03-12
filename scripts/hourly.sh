f7831d5986081f00a603047cafa9f1d0d9e41584
kupnu4x@gmail.com <kupnu4x@gmail.com>
2010-03-12 03:22:57 +0000

    hourly info

--- scripts/hourly.sh
+++ scripts/hourly.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 /usr/local/bin/php /usr/local/www/apache22/data/dcsearch/lister.php
 /usr/local/bin/php /usr/local/www/apache22/data/dcsearch/processer.php
-time -hl /usr/local/www/apache22/data/dcsearch/scripts/indexer.delta.sh > /dev/null
+time -h /usr/local/www/apache22/data/dcsearch/scripts/indexer.delta.sh > /dev/null
