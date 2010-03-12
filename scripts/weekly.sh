5baa1fbfee7cc760d2076a2e3bf2f0c478f3e974
kupnu4x@gmail.com <kupnu4x@gmail.com>
2010-03-12 08:25:22 +0000

    scritps logs

--- scripts/weekly.sh
+++ scripts/weekly.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 echo ==SPHINX INDEXER WEEKLY==`date "+%Y-%m-%d %H:%M:%S"`==
-/usr/bin/time -h /usr/local/www/apache22/data/dcsearch/scripts/indexer.delta.sh 2>&1 | tail -n 1
+/usr/bin/time -h /usr/local/www/apache22/data/dcsearch/scripts/indexer.main.sh 2>&1 | tail -n 1
