cb3bbba7aa972e5b72b82bd639fb7cc48b910b24
kupnu4x@gmail.com <kupnu4x@gmail.com>
2010-03-12 10:47:50 +0000

    scritps logs

--- scripts/hourly.sh
+++ scripts/hourly.sh
@@ -4,4 +4,4 @@ echo ==LISTER=================`date "+%Y-%m-%d %H:%M:%S"`==
 echo ==PROCESSER==============`date "+%Y-%m-%d %H:%M:%S"`==
 /usr/local/bin/php /usr/local/www/apache22/data/dcsearch/processer.php
 echo ==SPHINX INDEXER=========`date "+%Y-%m-%d %H:%M:%S"`==
-/usr/bin/time -h /usr/local/www/apache22/data/dcsearch/scripts/indexer.delta.sh 2>&1 | tail -n 1
+echo `/usr/bin/time -h /usr/local/www/apache22/data/dcsearch/scripts/indexer.delta.sh 2>&1 | tail -n 1`
--- scripts/weekly.sh
+++ scripts/weekly.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 echo ==SPHINX INDEXER WEEKLY==`date "+%Y-%m-%d %H:%M:%S"`==
-/usr/bin/time -h /usr/local/www/apache22/data/dcsearch/scripts/indexer.main.sh 2>&1 | tail -n 1
+echo `/usr/bin/time -h /usr/local/www/apache22/data/dcsearch/scripts/indexer.main.sh 2>&1 | tail -n 1`
